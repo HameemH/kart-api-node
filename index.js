634d655cb7ae8171d7fb53d2c1ff291e356e91aa
HameemH <hameemhossain2@gmail.com>
2022-07-18 23:10:24 +0600

    order section done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 app.use(cors());
@@ -69,18 +69,55 @@ async function run (){
           })
           app.put('/products/:id', async(req, res) =>{
             const id = req.params.id;
-            const updatedProduct = req.body
-            console.log(updatedUser);
+            const updatedProduct = req.body;
             const filter = {_id: ObjectId(id)};
             const options = { upsert: true };
             const updatedDoc = {
                 $set: updatedProduct
-                  
             };
             const result = await productsCollection.updateOne(filter, updatedDoc, options);
-            
             res.send(result);
     
+        })
+
+        /* Order Section */
+        app.post('/orders', async(req,res)=>{
+            const order =req.body;
+            const addOrder = await ordersCollection.insertOne(order)
+            res.send(addOrder)
+
+        })
+        app.get('/orders', async (req, res) => {
+            const query = {}
+            const cursor = ordersCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+          });
+            // Order of a specific customer filtered by email
+          app.get('/orders/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = {email:email}
+            const cursor = ordersCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+          });
+          app.put('/orders/:id', async(req,res) =>{
+            const id = req.params.id;
+            
+            const updatedOrder = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+              $set: updatedOrder,
+            };
+            const result = await ordersCollection.updateOne(filter, updateDoc, options);
+            res.send(result)
+        })
+        app.delete('/orders/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await ordersCollection.deleteOne(query);
+            res.send(result);
         })
    }
    finally{
